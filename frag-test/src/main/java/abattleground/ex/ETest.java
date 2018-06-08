18793c4d2a96fe58f39a00c3be6d4d5af667047b
wen3011 <627449989@qq.com>
2018-06-08 15:24:56 +0800

    merge

---
+++ frag-test/src/main/java/abattleground/ex/ETest.java
@@ -0,0 +1,26 @@
+package abattleground.ex;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ETest {
+
+    public static void main(String[] args) {
+
+//        final long per = 1000;
+//
+//        ScheduledExecutorService ss = Executors.newScheduledThreadPool(1);
+//
+//        ss.schedule(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("gggg");
+//            }
+//        }, per, TimeUnit.MILLISECONDS);
+
+        int n = 0;
+        System.out.println(++n);
+    }
+
+}
