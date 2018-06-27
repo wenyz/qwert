dd92d675a4691d034ed88137d1c530054c66d847
wen3011 <627449989@qq.com>
2018-06-27 11:10:25 +0800

    merge

--- frag-test/src/main/java/abattleground/ex/ETest.java
+++ frag-test/src/main/java/abattleground/ex/ETest.java
@@ -3,6 +3,9 @@ package abattleground.ex;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+import static java.lang.Thread.sleep;
 
 public class ETest {
 
@@ -19,8 +22,37 @@ public class ETest {
 //            }
 //        }, per, TimeUnit.MILLISECONDS);
 
-        int n = 0;
-        System.out.println(++n);
+//        int n = 0;
+//        System.out.println(++n);
+
+        Thread printThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+          //      while (true){
+                    System.out.println("print aaa aaa");
+                    try {
+                        sleep(10000);
+
+                        System.out.println("block the print thread");
+//                        LockSupport.park();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+        //        }
+            }
+        });
+
+        printThread.start();
+
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("now is here");
+        LockSupport.unpark(printThread);
+
     }
 
 }
---
+++ frag-test/src/main/java/abattleground/ocr/InnerClzz.java
@@ -0,0 +1,15 @@
+package abattleground.ocr;
+
+public class InnerClzz {
+
+    private String aaa;
+
+    static class Node{
+        static String ggg;
+
+
+        public Node() {
+            ggg="aaaa";
+        }
+    }
+}
